4f72677cd2e7985307318c9a37f858cdeafa15ba
brian.aziz <brian.aziz@pelindo.co.id>
2023-07-13 11:02:51 +0700

    slicing login, checkout, success checkout

--- app/Models/CampBenefit.php
+++ app/Models/CampBenefit.php
@@ -5,9 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class CampBenefits extends Model
+class CampBenefit extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['name'];
+    protected $fillable = ['camp_id','name'];
 }
